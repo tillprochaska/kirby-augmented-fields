8199433d55fe0b6c390d63ae8d8a4fb5e1bdcde5
Till Prochaska <mail@tillprochaska.de>
2022-01-29 21:57:50 +0100

    Do not augment fields for API and Panel requests

--- src/HasAugmentedFields.php
+++ src/HasAugmentedFields.php
@@ -2,11 +2,22 @@
 
 namespace TillProchaska\KirbyAugmentedFields;
 
+use Kirby\Cms\Content;
+use Kirby\Toolkit\Str;
+
 trait HasAugmentedFields
 {
-    public function content(string $langCode = null): AugmentedContent
+    public function content(string $langCode = null): Content
     {
         $content = parent::content($langCode);
+
+        $url = kirby()->request()->url();
+        $isPanelUrl = Str::startsWith($url, kirby()->url('panel'));
+        $isApiUrl = Str::startsWith($url, kirby()->url('api'));
+
+        if ($isPanelUrl || $isApiUrl) {
+            return $content;
+        }
 
         return new AugmentedContent($content->data(), $content->parent());
     }
